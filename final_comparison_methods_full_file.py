a7e00ec9386db82d2cfcec1c484ef525b00ad015
FranceRose <france.rose@wanadoo.fr>
2025-02-12 17:52:28 +0100

    minor correction to plots

--- final_comparison_methods_full_file.py
+++ final_comparison_methods_full_file.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     kpmoseq = pd.read_csv(
         os.path.join(basedir, f'results_behavior/outputs/25-09-24_FL2_new_for_comparison/DISK_test/test_for_optipose_repeat_0/kpmoseq/test_repeat-0_file{i_file}_kpmoseq.csv'))
     mbi = pd.read_csv(
-        os.path.join(basedir, f'results_behavior/MarkerBasedImputation_FL2/model_ensemble/test_repeat-0_merged/test_w-all-nans_file{i_file}_merged/test_w-all-nans_file{i_file}_sample0_MBI.csv'))
+        os.path.join(basedir, f'results_behavior/MarkerBasedImputation_FL2/model_ensemble/test_w-all-nans_file{i_file}_merged/test_w-all-nans_file{i_file}_sample0_MBI.csv'))
     n_methods = 4
     orig = pd.read_csv(
         os.path.join(basedir, f'results_behavior/outputs/25-09-24_FL2_new_for_comparison/DISK_test/test_for_optipose_repeat_0/test_w-all-nans_file{i_file}.csv'))
@@ -38,20 +38,17 @@ if __name__ == '__main__':
 
     fig, axes = plt.subplots(n_methods, 3, figsize=(18, 4), sharex='all', sharey='none')
     for j in range(3):
-        axes[0, j].plot(t, x[:, i_kp * 3 + j], 'o-', ms=1)
-        axes[1, j].plot(t, x[:, i_kp * 3 + j], 'o-', ms=1)
-        axes[2, j].plot(t, x[:, i_kp * 3 + j], 'o-', ms=1)
-        axes[2, j].plot(t, x[:, i_kp * 3 + j], 'o-', ms=1)
+        for i in range(n_methods):
+            axes[i, j].plot(t, x[:, i_kp * 3 + j], 'o-', ms=1)
 
         begins = np.where(np.diff(np.isnan(x[:, i_kp * 3 + j]).astype(int)) > 0)[0]
         ends = np.where(np.diff(np.isnan(x[:, i_kp * 3 + j]).astype(int)) < 0)[0]
         if np.isnan(x[0, i_kp * 3 + j]):
             begins = np.insert(begins, 0, 0)
         for b, e in zip(begins, ends):
-            axes[0, j].axvspan(xmin=t[b], xmax=t[e + 1], alpha=0.2)
-            axes[1, j].axvspan(xmin=t[b], xmax=t[e + 1], alpha=0.2)
-            axes[2, j].axvspan(xmin=t[b], xmax=t[e + 1], alpha=0.2)
-            axes[3, j].axvspan(xmin=t[b], xmax=t[e + 1], alpha=0.2)
+            for i in range(n_methods):
+                axes[i, j].axvspan(xmin=t[b], xmax=t[e + 1], alpha=0.2)
+
             axes[0, j].plot(t[b: e + 2], disk[b:e + 2, i_kp * 3 + j], '+--', c='red')
             axes[1, j].plot(t[b: e + 2], optipose.loc[b:e + 1, f'{i_kp}_{j + 1}'].values, '+--', c='red')
             axes[2, j].plot(t[b: e + 2], kpmoseq.loc[b:e + 1, f'{i_kp}_{j + 1}'].values, '+--', c='red')
@@ -105,14 +102,14 @@ if __name__ == '__main__':
                 ends = ends[1:]
             ends = np.insert(ends, len(ends) - 1, t1 - t0)
             for b, e in zip(begins, ends):
-                axes[0, j].plot(t[t0 + b: t0 + e + 1], disk[t0 + b: t0 + e + 1, i_kp * 3 + j], '+', alpha=0.4,
-                                c='orange')
+                # axes[0, j].plot(t[t0 + b: t0 + e + 1], disk[t0 + b: t0 + e + 1, i_kp * 3 + j], '+', alpha=0.4,
+                #                 c='orange')
                 axes[1, j].plot(t[t0 + b: t0 + e + 1], optipose.loc[t0 + b: t0 + e, f'{i_kp}_{j + 1}'].values, '+',
                                 alpha=0.4, c='orange')
                 axes[2, j].plot(t[t0 + b: t0 + e + 1], kpmoseq.loc[t0 + b: t0 + e, f'{i_kp}_{j + 1}'].values, '+',
                                 alpha=0.4, c='orange')
-                axes[3, j].plot(t[t0 + b: t0 + e + 1], mbi.loc[t0 + b: t0 + e, f'{i_kp}_{j + 1}'].values, '+',
-                                alpha=0.4, c='orange')
+                # axes[3, j].plot(t[t0 + b: t0 + e + 1], mbi.loc[t0 + b: t0 + e, f'{i_kp}_{j + 1}'].values, '+',
+                #                 alpha=0.4, c='orange')
         axes[0, 0].set_title('X')
         axes[0, 1].set_title('Y')
         axes[0, 2].set_title('Z')
@@ -131,7 +128,7 @@ if __name__ == '__main__':
        os.path.join(basedir, 'results_behavior/datasets/INH_FL2_keypoints_1_60_wresiduals_w1nan_stride0.5_new/test_fulllength_dataset_imputed.npz'))
     orig_npz = np.load(
        os.path.join(basedir, 'results_behavior/datasets/INH_FL2_keypoints_1_60_wresiduals_w1nan_stride0.5_new/test_fulllength_dataset_w-all-nans.npz'))
-    i_file = 8
+    i_file = 7
 
     x = orig_npz['X'][i_file]
     disk = imputed['X'][i_file]
@@ -157,7 +154,7 @@ if __name__ == '__main__':
     axes[0, 0].set_title('X')
     axes[0, 1].set_title('Y')
     axes[0, 2].set_title('Z')
-    plt.savefig(os.path.join(output_folder, 'test_w-all-nans_file7_DISK_holes_shaded.png'))
+    plt.savefig(os.path.join(output_folder, f'test_w-all-nans_file{i_file}_DISK_holes_shaded.png'))
 
     plt.close('all')
     optipose = pd.read_csv(
@@ -186,7 +183,7 @@ if __name__ == '__main__':
     axes[0, 0].set_title('X')
     axes[0, 1].set_title('Y')
     axes[0, 2].set_title('Z')
-    plt.savefig(os.path.join(output_folder, f'test_w-all-nans_file{i_file}_model_10_5_1_holes_shaded.png'))
+    plt.savefig(os.path.join(output_folder, f'test_w-all-nans_file{i_file}_OPTIPOSE_model_10_5_1_holes_shaded.png'))
 
     plt.close('all')
     optipose = pd.read_csv(
@@ -215,11 +212,11 @@ if __name__ == '__main__':
     axes[0, 0].set_title('X')
     axes[0, 1].set_title('Y')
     axes[0, 2].set_title('Z')
-    plt.savefig(os.path.join(output_folder, f'test_w-all-nans_file{i_file}_kpmoseq_holes_shaded.png'))
+    plt.savefig(os.path.join(output_folder, f'test_w-all-nans_file{i_file}_KPMOSEQ_holes_shaded.png'))
     plt.close()
 
     mbi = pd.read_csv(
-        os.path.join(basedir, f'results_behavior/MarkerBasedImputation_FL2/model_ensemble/test_repeat-0_merged/test_w-all-nans_file{i_file}_merged/test_w-all-nans_file{i_file}_sample0_MBI.csv'))
+        os.path.join(basedir, f'results_behavior/MarkerBasedImputation_FL2/model_ensemble/test_w-all-nans_file{i_file}_merged/test_w-all-nans_file{i_file}_sample0_MBI.csv'))
     orig = pd.read_csv(
         os.path.join(basedir, f'results_behavior/outputs/25-09-24_FL2_new_for_comparison/DISK_test/test_for_optipose_repeat_0/test_w-all-nans_file{i_file}.csv'))
     fig, axes = plt.subplots(8, 3, figsize=(50, 8 * 3), sharex='all', sharey='none')
@@ -244,4 +241,4 @@ if __name__ == '__main__':
     axes[0, 0].set_title('X')
     axes[0, 1].set_title('Y')
     axes[0, 2].set_title('Z')
-    plt.savefig(os.path.join(output_folder, f'test_w-all-nans_file{i_file}_mbi_holes_shaded.png'))
+    plt.savefig(os.path.join(output_folder, f'test_w-all-nans_file{i_file}_MBI_holes_shaded.png'))
