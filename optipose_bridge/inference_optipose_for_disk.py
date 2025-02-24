2c05af4ace2aecf5ab3dbe5e764f91b6f8e502b9
FranceRose <france.rose@wanadoo.fr>
2025-02-24 20:15:12 +0800

    rerun only fulllength

--- optipose_bridge/inference_optipose_for_disk.py
+++ optipose_bridge/inference_optipose_for_disk.py
@@ -37,13 +37,13 @@ output_dir = os.path.join(test_folder, 'optipose_pred')
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 
-for f in test_files:
-    print(f, os.path.basename(f).split(".")[0])
-    pred = FlattenedDataStore(config_.body_parts, f)
-    model.process(pred)
-    pred.save_file(os.path.join(output_dir, f'{os.path.basename(f).split(".")[0]}_model_{n_pcm}_{n_cm}_{n_heads}.csv'))
+# for f in test_files:
+#     print(f, os.path.basename(f).split(".")[0])
+#     pred = FlattenedDataStore(config_.body_parts, f)
+#     model.process(pred)
+#     pred.save_file(os.path.join(output_dir, f'{os.path.basename(f).split(".")[0]}_model_{n_pcm}_{n_cm}_{n_heads}.csv'))
 
-test_files = glob(os.path.join(test_folder, 'test_w-all-nans_file*.csv'))
+test_files = glob(os.path.join(test_folder, 'test_fulllength_dataset_w-all-nans_file*.csv'))
 for f in test_files:
     print(f)
     pred = FlattenedDataStore(config_.body_parts, f)
