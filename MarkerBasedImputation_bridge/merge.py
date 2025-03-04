77e61de4bd1e41e5bbfa5bc7d506e94d70e9d794
FranceRose <france.rose@wanadoo.fr>
2025-03-05 07:24:57 +0900

    debug

--- MarkerBasedImputation_bridge/merge.py
+++ MarkerBasedImputation_bridge/merge.py
@@ -111,7 +111,7 @@ def merge(save_path, pred_path, dataset_path):
     logging.info(f'shape markers: {markers.shape}')
     logging.info(f'shape member_stdsF: {member_stdsF.shape}')
     logging.info(f'shape predsF: {predsF.shape}')
-    logging.info(f'shape bad_framesF: {bad_framesF.shape}')
+    logging.info(f'shape bad_framesF: {bad_framesF.shape}, {np.all(bad_framesR == bad_framesF)}')
     logging.info(f'shape markers_means: {marker_means.shape}')
     logging.info(f'shape marker_stds: {marker_stds.shape}')
 
@@ -131,6 +131,7 @@ def merge(save_path, pred_path, dataset_path):
             x[get_mask(x, exclude_value)] = np.nan
             t = np.arange(markers.shape[1])
             axes[i].plot(x, 'o-')
+            axes[i].plot(t, predsF[item, :, i], 'x')
             axes[i].plot(t[bad_framesF[item, :, i].astype(bool)], predsF[item, bad_framesF[item, :, i].astype(bool), i], 'x')
             if i%divider == 0:
                 axes[i].set_ylabel(marker_names[i//divider])
@@ -144,7 +145,7 @@ def merge(save_path, pred_path, dataset_path):
             x[get_mask(x, exclude_value)] = np.nan
             t = np.arange(markers.shape[1])
             axes[i].plot(x, 'o-')
-            axes[i].plot(t[bad_framesR[item, :, i].astype(bool)], predsR[item, bad_framesF[item, :, i].astype(bool), i], 'x')
+            axes[i].plot(t[bad_framesR[item, :, i].astype(bool)], predsR[item, bad_framesR[item, :, i].astype(bool), i], 'x')
             if i%divider == 0:
                 axes[i].set_ylabel(marker_names[i//divider])
         plt.savefig(os.path.join(save_path, f'single_predR_pred_item-{item}_after_unprocess.png'))
@@ -181,7 +182,7 @@ def merge(save_path, pred_path, dataset_path):
                 where_predsF_is_nan = np.any(get_mask(predsF[sample, time_ids, kp * divider: kp * divider + divider], exclude_value), axis=-1)[:, np.newaxis]
                 weightR[where_predsR_is_nan] = 0
                 weightF = 1 - weightR
-                logging.info(f'MERGE, SUM WEIGHTS R: {np.sum(where_predsR_is_nan)}, SUM WEIGHTS F: {np.sum(where_predsF_is_nan)}, UNIQUE WEIGHTS F: {np.unique(weightF)}')
+                logging.info(f'MERGE, SUM WEIGHTS R: {np.sum(weightR)}, SUM WEIGHTS F: {weightF}, UNIQUE WEIGHTS F: {np.unique(weightF)}')
                 preds[sample, time_ids, kp * divider: kp * divider + divider] = predsF[sample, time_ids, kp * divider: kp * divider + divider] * weightF + predsR[sample, time_ids, kp * divider: kp * divider + divider] * weightR
                 member_stds[sample, time_ids, kp * divider: kp * divider + divider] = np.sqrt(member_stdsF[sample, time_ids, kp * divider: kp * divider + divider]**2 * weightF + member_stdsR[sample, time_ids, kp * divider: kp * divider + divider]**2 * weightR)
     elapsed = datetime.datetime.now() - start
