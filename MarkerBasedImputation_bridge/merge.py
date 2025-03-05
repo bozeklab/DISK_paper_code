14b4feaf1972afdec5cf2867889684740a4860d7
FranceRose <france.rose@wanadoo.fr>
2025-03-05 14:11:29 +0900

    debug

--- MarkerBasedImputation_bridge/merge.py
+++ MarkerBasedImputation_bridge/merge.py
@@ -126,7 +126,7 @@ def merge(save_path, pred_path, dataset_path):
             x[mask_x] = np.nan
             t = np.arange(markers.shape[1])
             axes[i].plot(t, predsF[item, :, i], 'x')
-            axes[i].plot(t[mask_x], predsF[item, mask_x, i], 'x')
+            axes[i].plot(t[~mask_x], predsF[item, ~mask_x, i], 'x')
             if i%divider == 0:
                 axes[i].set_ylabel(marker_names[i//divider])
         plt.savefig(os.path.join(save_path, f'single_predF_pred_item-{item}_before_unprocess.png'))
@@ -140,7 +140,7 @@ def merge(save_path, pred_path, dataset_path):
             x[mask_x] = np.nan
             t = np.arange(markers.shape[1])
             axes[i].plot(x, 'o-')
-            axes[i].plot(t[mask_x], predsR[item, mask_x, i], 'x')
+            axes[i].plot(t[~mask_x], predsR[item, ~mask_x, i], 'x')
             if i%divider == 0:
                 axes[i].set_ylabel(marker_names[i//divider])
         plt.savefig(os.path.join(save_path, f'single_predR_pred_item-{item}_before_unprocess.png'))
@@ -166,7 +166,7 @@ def merge(save_path, pred_path, dataset_path):
             t = np.arange(markers.shape[1])
             axes[i].plot(x, 'o-')
             # axes[i].plot(t, predsF[item, :, i], 'x')
-            axes[i].plot(t[mask_x], predsF[item, mask_x, i], 'x')
+            axes[i].plot(t[~mask_x], predsF[item, ~mask_x, i], 'x')
             if i%divider == 0:
                 axes[i].set_ylabel(marker_names[i//divider])
         plt.savefig(os.path.join(save_path, f'single_predF_pred_item-{item}_after_unprocess.png'))
@@ -180,7 +180,7 @@ def merge(save_path, pred_path, dataset_path):
             x[mask_x] = np.nan
             t = np.arange(markers.shape[1])
             axes[i].plot(x, 'o-')
-            axes[i].plot(t[mask_x], predsR[item, mask_x, i], 'x')
+            axes[i].plot(t[~mask_x], predsR[item, ~mask_x, i], 'x')
             if i%divider == 0:
                 axes[i].set_ylabel(marker_names[i//divider])
         plt.savefig(os.path.join(save_path, f'single_predR_pred_item-{item}_after_unprocess.png'))
@@ -231,7 +231,7 @@ def merge(save_path, pred_path, dataset_path):
             x[mask_x] = np.nan
             t = np.arange(markers.shape[1])
             axes[i].plot(x, 'o-')
-            axes[i].plot(t[mask_x], preds[item, mask_x, i], 'x')
+            axes[i].plot(t[~mask_x], preds[item, ~mask_x, i], 'x')
             if i%divider == 0:
                 axes[i].set_ylabel(marker_names[i//divider])
         plt.savefig(os.path.join(save_path, f'single_predMerged_pred_item-{item}_after_unprocess.png'))
